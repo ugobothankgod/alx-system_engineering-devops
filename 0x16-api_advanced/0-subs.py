5aeea49ce5e16fa02a955437fc2d1c3692988b5b
ugoMusk <ugobothankgod@gmail.com>
2024-01-18 23:46:41 +0100

    disable redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    response = get(url, headers=user_agent, allow_redirects=False)
     results = response.json()
 
     try:
